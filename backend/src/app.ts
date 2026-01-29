21569475e83777f2d1941cbdad95527445bb9543
A1exanderr <looveyouuu10@gmail.com>
2026-01-29 14:35:30 -0400

    desarrollo login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import Fastify from 'fastify'
-
+import "dotenv/config";
 const fastify = Fastify({
   logger: true
 })
@@ -7,10 +7,13 @@ const fastify = Fastify({
 fastify.get('/', async (request, reply) => {
   return { mensaje: 'Hola Mundo desde Fastify + TypeScript 👋' }
 })
-
+/* fastify.get('/', async () => {
+  return { mensaje: 'Hola desde alkey' }
+}) */
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' })
+    const port = Number(process.env.PORT) || 3000
+    await fastify.listen({ port, host: '0.0.0.0' })
     console.log('Servidor corriendo en http://localhost:3000')
   } catch (err) {
     fastify.log.error(err)
