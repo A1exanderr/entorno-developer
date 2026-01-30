05c0a7bf5a030c24203c6661b111931890899779
A1exanderr <looveyouuu10@gmail.com>
2026-01-30 14:39:20 -0400

    desarrollo instalacion y con 8 vunerabilidades bcrypt

--- backend/prisma/seeders/usuarioSeeder.ts
+++ backend/prisma/seeders/usuarioSeeder.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client'
+/* import { PrismaClient } from '../generated/prisma/client'
 import bcrypt from 'bcrypt'
 
 export async function seedUsuarios(prisma: PrismaClient) {
@@ -33,5 +33,54 @@ export async function seedUsuarios(prisma: PrismaClient) {
     skipDuplicates: true,
   })
 
+  console.log('Usuarios creados')
+}
+ */
+import { PrismaClient, Estado } from '../generated/prisma/client'
+import bcrypt from 'bcrypt'
+
+export async function seedUsuarios(prisma: PrismaClient) {
+  console.log('Creando usuarios...')
+  const salt = 12
+  const usuariosRaw = [
+    {
+      nombre: 'Alex',
+      a_paterno: 'Mamani',
+      a_materno: 'Llojlla',
+      ci: '12345678',
+      email: 'developer@gmail.com',
+      password: 'password',
+      foto: 'avatar.png',
+      estado: Estado.activo,
+    },
+    {
+      nombre: 'María',
+      a_paterno: 'López',
+      a_materno: 'Martínez',
+      ci: '87654321',
+      email: 'b@b.com',
+      password: 'password',
+      foto: 'avatar.png',
+      estado: Estado.activo,
+    },
+  ]
+
+  const usuarios = await Promise.all(
+    usuariosRaw.map(async u => ({
+      nombre: u.nombre,
+      a_paterno: u.a_paterno,
+      a_materno: u.a_materno,
+      ci: u.ci,
+      email: u.email,
+      password: await bcrypt.hash(u.password, salt),
+      foto: u.foto,
+      estado: u.estado,
+    }))
+  )
+
+  await prisma.usuario.createMany({ 
+    data: usuarios,
+    skipDuplicates: true,
+  })
   console.log('Usuarios creados')
 }
