17c1d313524e6f6d04bb5578f6eb0d21501a17f6
A1exanderr <looveyouuu10@gmail.com>
2026-02-03 16:08:33 -0400

    desarrollo

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Login } from './components/login/login';
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        component: Login,
+    },
+    /* {
+        path: 'dashboard',
+        component: Dashboard,
+        canActivate: [authGuard],
+    }, */
+    {
+        path: '**',
+        redirectTo: 'login',
+    },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { ButtonModule } from 'primeng/button';
-//importar componentes
-import { Login } from './components/login/login';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, Login],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
