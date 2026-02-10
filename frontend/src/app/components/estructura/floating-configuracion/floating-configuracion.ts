2ecabd06801c354ca4643c13b777e8a54afd53eb
A1exanderr <looveyouuu10@gmail.com>
2026-02-10 09:58:25 -0400

    desarrollo

---
+++ frontend/src/app/components/estructura/floating-configuracion/floating-configuracion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-configuracion',
+  imports: [],
+  templateUrl: './floating-configuracion.html',
+  styleUrl: './floating-configuracion.scss',
+})
+export class FloatingConfiguracion {
+
+}
