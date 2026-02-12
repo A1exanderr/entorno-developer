77937b5668bfa2ca77eaf0c5a17e1019d5a1710d
A1exanderr <looveyouuu10@gmail.com>
2026-02-12 14:04:20 -0400

    desarrollo rutas

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -56,8 +56,8 @@ export class Login {
         this.mensaje = 'Login exitoso';
         //console.log(resp);
         // navegar al dashboard
-        //this.router.navigate(['/panel']);
-        this.router.navigate(['/base']);
+        this.router.navigate(['/panel']);
+        //this.router.navigate(['/base']);
       },
       error: (err) => {
         //console.log("alkey",err.error?.message);
