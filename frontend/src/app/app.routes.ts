25a7be3618b7df0f828260080f20d4f7056b99e7
A1exanderr <looveyouuu10@gmail.com>
2026-02-12 12:20:24 -0400

    desarrollo roles

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+/* import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Panel } from './components/panel/panel';
 import { Layout } from './components/estructura/layout/layout';
@@ -27,4 +27,38 @@ export const routes: Routes = [
         path: '**',
         redirectTo: 'login',
     },
+];
+ */
+import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Panel } from './components/panel/panel';
+import { Layout } from './components/estructura/layout/layout';
+import { authGuard } from './core/guards/auth-guard'
+export const routes: Routes = [
+
+    // 🔓 Login sin layout
+    {
+        path: 'login',
+        component: Login
+    },
+
+    // 🔐 Todo lo protegido usa Layout
+    {
+        path: '',
+        component: Layout,
+        canActivate: [authGuard],
+        children: [
+            { path: 'panel', component: Panel },
+            // aquí irán todas tus páginas protegidas
+            // { path: 'usuarios', component: Usuarios },
+            // { path: 'roles', component: Roles },
+            { path: '', redirectTo: 'panel', pathMatch: 'full' }
+        ]
+    },
+
+    // fallback
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
 ];
