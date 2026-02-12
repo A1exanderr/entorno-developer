e438321932b5549568abedf9bb0bb93fd0607bc1
A1exanderr <looveyouuu10@gmail.com>
2026-02-12 18:33:19 -0400

    desarrollo frontend

--- frontend/src/app/components/roles/roles.ts
+++ frontend/src/app/components/roles/roles.ts
@@ -1,10 +1,14 @@
 import { Component, inject, ChangeDetectorRef } from '@angular/core';
 //importtar servicio
 import { RolesService } from '../../servicios/features/roles.service';
+//ngprime
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [CommonModule, TableModule, ButtonModule],
   templateUrl: './roles.html',
   styleUrl: './roles.scss',
 })
@@ -14,16 +18,32 @@ export class Roles {
 
   public datos_roles: any[] = [];
   ngOnInit() {
-        /* this.menuService.menu().subscribe({
-        next: (resp: any) => {
-            console.log(resp);
-            this.model = resp;
-            this.cd.detectChanges(); 
-        },
-        error: (err) => {
-            console.log(err.error?.message || 'Datos incorrectos');
-        }
-        }); */
-      console.log("aqui");
-    }
+    this.rolesService.listar().subscribe({
+      next: (resp: any) => {
+        //console.log(resp);
+        this.datos_roles = resp;
+        this.cd.detectChanges(); 
+      },
+      error: (err) => {
+        console.log(err.error?.message || 'Datos incorrectos');
+      }
+    });
+  }
+
+  public editarRol(id:number)
+  {}
+  public eliminarRol(id:number)
+  {
+    console.log(id);
+    this.rolesService.eliminar({id}).subscribe({
+      next: (resp: any) => {
+        //console.log(resp);
+        this.datos_roles = resp;
+        this.cd.detectChanges(); 
+      },
+      error: (err) => {
+        console.log(err.error?.message || 'Datos incorrectos');
+      }
+    });
+  }
 }
