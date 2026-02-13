52eb5644611f790054b7f4971aec33b5d8e3630d
A1exanderr <looveyouuu10@gmail.com>
2026-02-13 09:16:59 -0400

    desarrollo frontend

--- frontend/src/app/components/roles/roles.ts
+++ frontend/src/app/components/roles/roles.ts
@@ -1,14 +1,16 @@
-import { Component, inject, ChangeDetectorRef } from '@angular/core';
+import { Component, inject, ChangeDetectorRef, signal } from '@angular/core';
 //importtar servicio
 import { RolesService } from '../../servicios/features/roles.service';
 //ngprime
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
+//ng-prime editar
+import { Dialog, DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-roles',
-  imports: [CommonModule, TableModule, ButtonModule],
+  imports: [CommonModule, TableModule, ButtonModule, DialogModule],
   templateUrl: './roles.html',
   styleUrl: './roles.scss',
 })
@@ -16,12 +18,14 @@ export class Roles {
   private rolesService = inject(RolesService);
   private cd = inject(ChangeDetectorRef);
 
-  public datos_roles: any[] = [];
+  //public datos_roles: any[] = [];
+  public datos_roles = signal<any[]>([]);
   ngOnInit() {
     this.rolesService.listar().subscribe({
       next: (resp: any) => {
         //console.log(resp);
-        this.datos_roles = resp;
+        //this.datos_roles = resp;
+        this.datos_roles.set(resp);
         this.cd.detectChanges(); 
       },
       error: (err) => {
@@ -30,16 +34,17 @@ export class Roles {
     });
   }
 
-  public editarRol(id:number)
-  {}
   public eliminarRol(id:number)
   {
-    console.log(id);
+    //console.log(id);
     this.rolesService.eliminar({id}).subscribe({
       next: (resp: any) => {
+        /* this.datos_roles = this.datos_roles.filter(r => r.id !== id);
+        this.cd.detectChanges(); */
+        this.datos_roles.update(roles =>
+          roles.filter(r => r.id !== id)
+        );
         //console.log(resp);
-        this.datos_roles = resp;
-        this.cd.detectChanges(); 
       },
       error: (err) => {
         console.log(err.error?.message || 'Datos incorrectos');
