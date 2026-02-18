8d3132d7c5664ff5a546427fd45642d76f99cc07
A1exanderr <looveyouuu10@gmail.com>
2026-02-18 08:58:02 -0400

    iniciando

--- backend/src/plugins/cookies.ts
+++
@@ -1,9 +0,0 @@
-import fp from 'fastify-plugin'
-import cookie from '@fastify/cookie'
-
-export default fp(async (fastify) => {
-  fastify.register(cookie, {
-    secret: process.env.COOKIE_SECRET || 'supersecret',
-    hook: 'onRequest',
-  })
-})
--- backend/src/types/fastify.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '../../prisma/generated/prisma/client'
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    prisma: PrismaClient
-  }
-}
